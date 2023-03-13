14b29c54e1061b680ac17c81c8b466b14d532b90
Florian Thiel <flth@flth.de>
2023-03-13 23:21:24 +0100

    initial

---
+++ mod.lua
@@ -0,0 +1,24 @@
+function data()
+return {
+	info = {
+		minorVersion = 2,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+			{
+				name = "ModWerkstatt",
+				role = "CREATOR",
+			},
+		},
+		tags = { "Europe", "truck", "lorry", "lkw", },
+	},
+	options = {
+		},
+
+		runFn = function (settings)
+
+		end
+	}
+end
